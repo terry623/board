feb9dfcb49676a7952ecbeac81f50210b629129a
Terry Lin <terry92516@gmail.com>
2020-04-01 17:29:09 +0800

    connect db

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => '127.0.0.1',
+	'username' => 'root',
+	'password' => 'root',
+	'database' => 'mysql',
+	'port'     => '3306',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
